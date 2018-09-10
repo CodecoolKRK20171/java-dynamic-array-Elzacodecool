d1db4bcf13aee50ea1dcfb0d72fcee9753c868b5
Elzacodecool <elliegolec@gmail.com>
2018-09-10 12:57:50 +0200

    Add constructors

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,5 +2,13 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
+    private int[] array;
 
+    public DynamicIntArray() {
+        array = new int[5];
+    }
+
+    public DynamicIntArray(int length) {
+        array = new int[length];
+    }
 }
